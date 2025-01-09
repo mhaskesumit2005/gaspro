83c0d137b4a1ff4c08746c8b30376c3f1f63a4e6
Sumit Harishchandra Mhaske <sumitmhaske435@gmail.com>
2025-01-09 11:04:19 +0530

    update

--- application/views/master/profile.php
+++ application/views/master/profile.php
@@ -2,10 +2,10 @@
     <div class="col-md-12">
         <ul class="nav nav-pills flex-column flex-md-row mb-3">
             <li class="nav-item">
-                <a class="nav-link active" href="<?= base_url() ?>master/profile"><i class="bx bx-user me-1"></i> Profile</a>
+                <a class="nav-link active" href="<?= base_url() ?>master/profile"><i class="bx bx-user me-2"></i> Profile</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="<?= base_url() ?>master/setting"><i class='bx bxs-lock bx-tada'></i> Security</a>
+                <a class="nav-link" href="<?= base_url() ?>master/setting"><i class='bx bxs-lock bx-tada me-2'></i> Security</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#"><i class="bx bx-link-alt me-1"></i> Connections</a>
@@ -29,7 +29,7 @@
             </div>
             <hr class="my-0" />
             <div class="card-body">
-                <form id="formAccountSettings" method="POST" onsubmit="return false">
+                <form id="formAccountSettings" method="post" >
                     <div class="row">
                         <div class="mb-3 col-md-6">
                             <label for="business_name" class="form-label">Business Name</label>
@@ -68,42 +68,10 @@
                                 <input class="form-control" type="file" id="business_logo" name="business_logo" />
                             </div>
                         </div>
-                        <div class="mb-3 col-md-6">
-                            <label for="state" class="form-label"></label>
-                            <input class="form-control" type="text" id="state" name="state" placeholder="California" />
-                        </div>
-                        <div class="mb-3 col-md-6">
-                            <label for="zipCode" class="form-label">Zip Code</label>
-                            <input type="text" class="form-control" id="zipCode" name="zipCode" placeholder="231465" maxlength="6" />
-                        </div>
                     </div>
                     <div class="mt-2">
-                        <button type="submit" class="btn btn-primary me-2">Save changes</button>
-                        <button type="reset" class="btn btn-outline-secondary">Cancel</button>
+                        <button class="btn btn-secondary me-2">Save changes</button>
                     </div>
-                </form>
-            </div>
-            <!-- /Account -->
-        </div>
-        <div class="card">
-            <h5 class="card-header">Delete Account</h5>
-            <div class="card-body">
-                <div class="mb-3 col-12 mb-0">
-                    <div class="alert alert-warning">
-                        <h6 class="alert-heading fw-bold mb-1">Are you sure you want to delete your account?</h6>
-                        <p class="mb-0">Once you delete your account, there is no going back. Please be certain.</p>
-                    </div>
-                </div>
-                <form id="formAccountDeactivation" onsubmit="return false">
-                    <div class="form-check mb-3">
-                        <input
-                            class="form-check-input"
-                            type="checkbox"
-                            name="accountActivation"
-                            id="accountActivation" />
-                        <label class="form-check-label" for="accountActivation">I confirm my account deactivation</label>
-                    </div>
-                    <button type="submit" class="btn btn-danger deactivate-account">Deactivate Account</button>
                 </form>
             </div>
         </div>
--- application/views/master/setting.php
+++ application/views/master/setting.php
@@ -1,13 +1,11 @@
-<div class="container-xxl flex-grow-1 container-p-y">
-
     <div class="row">
         <div class="col-md-12">
             <ul class="nav nav-pills flex-column flex-md-row mb-3">
                 <li class="nav-item">
-                    <a class="nav-link" href="<?= base_url() ?>master/profile"><i class="bx bx-user me-1"></i> Profile</a>
+                    <a class="nav-link" href="<?= base_url() ?>master/profile"><i class="bx bx-user me-2"></i> Profile</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="<?= base_url() ?>master/setting"><i class='bx bxs-lock bx-tada'></i> Security</a>
+                    <a class="nav-link active" href="<?= base_url() ?>master/setting"><i class='bx bxs-lock bx-tada me-2'></i> Security</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#"><i class="bx bx-link-alt me-1"></i> Connections</a>
